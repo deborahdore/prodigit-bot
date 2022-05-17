e9af9fa3e162cdcc81559e8bcd50d180ae09691c
SuppahDent <61562770+SuppahDent@users.noreply.github.com>
2022-05-17 19:31:22 +0200

    Add files via upload
    Al momento ci sono un sacco di istruzioni inutili, ma per il momento ho salvato solo queste cose.

---
+++ trials/LibTelegram.py
@@ -0,0 +1,35 @@
+import logging
+from telegram import *
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, CallbackContext
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: CallbackContext.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+async def rispondimale(update: Update, context: CallbackContext.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="we...a fess' e mammt'!")
+
+async def book(update: Update, context: CallbackContext.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id,text="Che cosa vorresti prenotare?")
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token('5214072158:AAGj-lZig1CmTn06nI7JBiTH7nbm1grlv_I').build()
+
+
+
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), rispondimale)
+    buttons = ['Start', 'Settings', 'Back']
+    markup = ReplyKeyboardMarkup.from_column(buttons)
+    MessageHandler(filters.Text(buttons), book)
+    
+    start_handler = CommandHandler('hi', start)
+    application.add_handler(start_handler)
+    application.add_handler(CommandHandler('strunz', rispondimale))
+    application.add_handler(CommandHandler('book', book))
+               
+    application.run_polling()
