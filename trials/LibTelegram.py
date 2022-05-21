d983a18f4d391f3f4d5e79ee17c87a4c677b654d
Deborah Dore <dore.1994616@studenti.uniroma1.it>
2022-05-21 12:42:15 +0200

    codice parallelo

--- trials/LibTelegram.py
+++ trials/LibTelegram.py
@@ -1,60 +1,67 @@
-import logging
-import requests as re
-from telegram import *
-from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, CallbackContext
+import threading
+import time
 
+import requests
 
-"""logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
-
-async def start(update: Update, context: CallbackContext.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-
-async def rispondimale(update: Update, context: CallbackContext.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="we...a fess' e mammt'!")
-
-async def book(update: Update, context: CallbackContext.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id,text="Che cosa vorresti prenotare?")
-
-if __name__ == '__main__':
-    application = ApplicationBuilder().token('5214072158:AAGj-lZig1CmTn06nI7JBiTH7nbm1grlv_I').build()
-
-
-
-    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), rispondimale)
-    buttons = ['Start', 'Settings', 'Back']
-    markup = ReplyKeyboardMarkup.from_column(buttons)
-    MessageHandler(filters.Text(buttons), book)
-    
-    start_handler = CommandHandler('hi', start)
-    application.add_handler(start_handler)
-    application.add_handler(CommandHandler('strunz', rispondimale))
-    application.add_handler(CommandHandler('book', book))
-               
-    application.run_polling()"""
-
-botoken='5214072158:AAGj-lZig1CmTn06nI7JBiTH7nbm1grlv_I'
-endpoint = 'https://api.telegram.org/bot'+botoken+'/'
-initbot = re.get(endpoint+'getme')
+botoken = '5214072158:AAGj-lZig1CmTn06nI7JBiTH7nbm1grlv_I'
+endpoint = 'https://api.telegram.org/bot' + botoken + '/'
+message_buffer = []
 last = 0
+mutex = threading.Semaphore()
 
-def update():
-    messages = re.get(endpoint+'getUpdates').json()
-    for i in range(messages['result']):
-        if(messages['result'][i]['update_id'] > last):
-            last = messages['result'][i]['update_id']
-            return messages['result'][i:]
+
+class Producer(threading.Thread):
+    def run(self):
+        global message_buffer
+        global mutex
+        global last
+
+        mutex.acquire()
+        messages = requests.get(endpoint + 'getUpdates').json()
+        for i in range(len(messages['result'])):
+
+            if int(messages['result'][i]['update_id']) > last:
+                message_buffer.extend(messages['result'][i:])
+
+                # update last
+                last = message_buffer[-1]['update_id']
+
+                break
+
+        mutex.release()
+        time.sleep(0.1)
+
+
+class Consumer(threading.Thread):
+    def run(self):
+        global message_buffer
+        global mutex
+
+        mutex.acquire()
+
+        if len(message_buffer) > 0:
+            while len(message_buffer) != 0:
+                message = message_buffer.pop(0)
+                requests.post(
+                    url=f'{endpoint}sendMessage',
+                    data={'chat_id': message['message']['chat']['id'], 'text': 'hello friend'},
+
+                )
+
+        mutex.release()
+        time.sleep(0.1)
+
 
 if __name__ == '__main__':
-    messages = []
     while True:
-        if(len(messages)==0):
-            messages.append(update)
+        # Creating Threads
+        producer = Producer()
+        consumer = Consumer()
 
-        if(len(messages)!=0):
-            for i in messages:
-                messages.remove(i)
+        # Starting Threads
+        consumer.start()
+        producer.start()
 
-#print(initbot.json())
+        # Waiting for threads to complete
+        producer.join()
+        consumer.join()
--- trials/prodigit_merda.py
+++ trials/prodigit_merda.py
@@ -1,39 +1,37 @@
 import requests
 from requests.structures import CaseInsensitiveDict
 
-
 login_url = "https://prodigit.uniroma1.it/names.nsf?Login"
 logout_url = "https://prodigit.uniroma1.it/prenotazioni/prenotaaule.nsf?logout"
 prenota_lezioni_url = "https://prodigit.uniroma1.it/prenotazioni/prenotaaule.nsf/prenotaposto-aula-lezioni"
 
-
 credentials = {'Username': "qua",
-            'Password': "qua"}
+               'Password': "qua"}
 
 headers = CaseInsensitiveDict()
-headers["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+headers[
+    "Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
 headers["Connection"] = "keep-alive"
 headers["Content-Type"] = "application/x-www-form-urlencoded"
 
 # LOGIN 
 
-login = requests.post(login_url, data = credentials, headers = headers)
+login = requests.post(login_url, data=credentials, headers=headers)
 
-headers["Cookie"] = "LtpaToken="+login.request.headers['Cookie'][10:]
-
+headers["Cookie"] = "LtpaToken=" + login.request.headers['Cookie'][10:]
 
 # PRENOTA AULA
 
 click = "__Click=C12585E7003519C8.c8e9f943d3b2819fc12587ed0064a0a2%2F%24Body%2F2.9F0"
-codice_edificio="codiceedificio=RM018"
-aula="aula=AULA+1+--+RM018-E01PTEL013"
-dalleore="dalleore2=08%3A00"
-alleore="alleore2=09%3A00"
+codice_edificio = "codiceedificio=RM018"
+aula = "aula=AULA+1+--+RM018-E01PTEL013"
+dalleore = "dalleore2=08%3A00"
+alleore = "alleore2=09%3A00"
 
 parameters = [click, codice_edificio, aula, dalleore, alleore]
 data = ""
 for param in parameters:
-    data+=param+"&"
+    data += param + "&"
 
 resp = requests.post(prenota_lezioni_url, headers=headers, data=data)
 
